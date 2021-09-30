3b0b85be31dd40e04daf7809eacd1c83629fb8cd
RobGro <r.grodzki74@gmail.com>
2021-09-30 13:45:09 +0100

    Initial commit

---
+++ src/main/java/com/robgro/factorauthorization/FactorAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.robgro.factorauthorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactorAuthorizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FactorAuthorizationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/robgro/factorauthorization/FactorAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robgro.factorauthorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactorAuthorizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
