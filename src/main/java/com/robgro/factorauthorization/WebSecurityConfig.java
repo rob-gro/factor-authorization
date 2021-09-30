1e1b56fbcf1d3573ab90e116c2cd5b4585080f97
RobGro <r.grodzki74@gmail.com>
2021-09-30 16:49:19 +0100

    added Password Encoder

--- src/main/java/com/robgro/factorauthorization/WebSecurityConfig.java
+++ src/main/java/com/robgro/factorauthorization/WebSecurityConfig.java
@@ -1,13 +1,22 @@
 package com.robgro.factorauthorization;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 // 1. klasa konfigurująca całą aplikację
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter { // --> zestaw implementacji security
+
+    // 10 . przechowywanie hasła użytkownika w postaci zaszyfrowanej
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     // 9. wstrzykujemy
     private UserDetailsServiceImpl userDetailsService;
