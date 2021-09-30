ad8ceb0b90fd0e82e3155abc765faaf0a29126fa
RobGro <r.grodzki74@gmail.com>
2021-09-30 15:49:51 +0100

    added AppUser class

---
+++ src/main/java/com/robgro/factorauthorization/AppUser.java
@@ -0,0 +1,60 @@
+package com.robgro.factorauthorization;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Collection;
+
+// 2.  klasa tworząca użytkowników
+@Entity     // utworzenie encji w bazie danych
+public class AppUser implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // automatyczne inkrementowanie Id
+    private Long id;
+
+    private String userName;
+    private String password;
+    private String role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
